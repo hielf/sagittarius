6080925fd90fb6c1d59300a0ada2f6e3eeaebbd6
moumou <hielf@qq.com>
2018-02-04 00:49:24 +0800

    update readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(username: 'admin', password: '123456', role: 'admin', station_id: station.id)
+User.create(username: 'admin', password: '123456', role: 'admin')
