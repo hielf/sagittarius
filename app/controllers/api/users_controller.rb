7a54d6bf6545b31477317fdf1c7320e47844961b
moumou <hielf@qq.com>
2018-02-08 15:07:25 +0800

    test

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < Api::ApplicationController
   wechat_api
   skip_before_action :authenticate_user!, only: [:index, :home, :outworker_new, :staff_new, :create, :teams, :areas, :shops]
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :initial_user, only: [:create]
+  before_action :initial_user, only: [:outworker_new, :staff_new]
   before_action only: [:destroy] { render_json([403, t('messages.c_403')]) if current_user.role != 'admin' }
 
   def home
