cbbd6befc5c971deb61217ba617cd169d14495a5
moumou <hielf@qq.com>
2018-02-10 15:59:51 +0800

    added qiniu token

--- app/controllers/api/qiniu_controller.rb
+++ app/controllers/api/qiniu_controller.rb
@@ -1,13 +1,17 @@
 class Api::QiniuController < Api::ApplicationController
   require 'qiniu'
   def token
-    Qiniu.establish_connection! access_key: ENV['qiniu_key'],
-                                secret_key: ENV['qiniu_secret']
+    Qiniu.establish_connection! access_key: ENV['qiniu_ak'],
+                                secret_key: ENV['qiniu_sk']
     bucket = ENV["qiniu_bucket"]
     put_policy = Qiniu::Auth::PutPolicy.new(bucket)
     # 生成上传 Token
     upload_token = Qiniu::Auth.generate_uptoken(put_policy)
-    result = [0, upload_token]
+    if upload_token
+      result = [0, upload_token]
+    else
+      result = [1, "failed"]
+    end
     render_json(result)
   end
 end
