46753963480d1a9cac5272c04329b64f4201b85d
moumou <hielf@qq.com>
2018-02-12 13:41:44 +0800

    test qiniu

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,2 @@
-// var root_url='http://localhost:3000/';
-var root_url='http://sagittarius.cheshipin.tv/';
+var root_url='http://10.0.0.17:3000/';
+// var root_url='http://sagittarius.cheshipin.tv/';
