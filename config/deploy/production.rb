e70e2df99ba990e24178283f069ffc11dccaeacc
moumou <hielf@qq.com>
2018-03-27 03:28:02 +0800

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,6 +11,7 @@
 server '47.97.191.42', port: 22, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
 set :branch, "master"
 set :rvm_ruby_version, '2.4.0@sagittarius'
+# set :rvm_type, :system
 
 # role-based syntax
 # ==================
