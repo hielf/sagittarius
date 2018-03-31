6a9f54ea93acdce968ba03ae51701f8af252d711
dreamlinx <dreamlinx@gmail.com>
2018-03-31 16:00:37 +0800

    state 改为 动态

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-permit_params :username, :openid, :password,
+permit_params :username, :password,
   :mobile, :role, :area_id, :shop_id, :upper_user_id,
   :upper_user_phone, :team_id, :access_token, :sign_in_count,
   :current_sign_in_at, :current_sign_in_ip, :last_sign_in_at,
