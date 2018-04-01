6ab582912001805c264601ab504f53fdd29540f4
moumou <hielf@qq.com>
2018-04-02 01:28:48 +0800

    test

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -47,14 +47,14 @@ class Api::StatesController < Api::ApplicationController
       order = 1
       serial_code = current_user.username.to_s + Time.now.strftime('%Y%m%d%H%M%s')
       params[:photos][:image].each do |image|
-        Photo.transaction do
-          photo = @state.photos.new(user_id: current_user.id, event_id: params[:event_id], image: image[0], media_id: image[1], order: order, serial_code: serial_code)
-          photo.save!
-          photo.trans_wechat_media
-          order = order + 1
-        end
-        @state.save!
+        # Photo.transaction do
+        photo = @state.photos.new(user_id: current_user.id, event_id: params[:event_id], image: image[0], media_id: image[1], order: order, serial_code: serial_code)
+        photo.save!
+        order = order + 1
+        # end
+        photo.trans_wechat_media
       end
+      @state.save!
     rescue Exception => ex
       result= [1, ex.message]
     end
